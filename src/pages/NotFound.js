a466e7915960cfe85934138d729ec433209b8885
unknown <oliwerklauze22@gmail.com>
2021-07-30 10:30:04 +0200

    notfound page little fixed but it has to be redisigned

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 export default function NotFound() {
   return (
-    <div>
+    <div
+      style={{
+        width: '100%',
+        color: '#fff',
+        display: 'grid',
+        placeItems: 'center',
+      }}
+    >
       <h1>There is no that page</h1>
     </div>
   );
