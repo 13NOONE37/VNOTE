037ba3587b9343ef10b5aa1affd3b0fa9f06b2ff
13NOONE37 <oliwerklauze22@gmail.com>
2021-08-12 23:23:12 +0200

    sidebar started

---
+++ src/components/main/ControPageCount.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import 'css/main/ControlPageCount.css';
+
+function ControPageCount() {
+  return (
+    <div className='controlPageCount'>
+      <button className='skewButton'>
+        <i className='fas fa-backward'></i>
+      </button>
+      <button>
+        <i className='fas fa-angle-left'></i>
+      </button>
+      <input type='number' min={0} />
+      <button>
+        <i className='fas fa-angle-right'></i>
+      </button>
+      <button className='skewButton'>
+        <i className='fas fa-forward'></i>
+      </button>
+    </div>
+  );
+}
+
+export default ControPageCount;
--- src/pages/NotebookEditPage.js
+++ src/pages/NotebookEditPage.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router';
 import AppContext from 'store/appContext';
 import 'css/main/notebookPage.css';
 import 'css/main/SideBar.css';
+import ControPageCount from 'components/main/ControPageCount';
 
 export default function NotebookEditPage() {
   const { id } = useParams();
@@ -17,10 +18,108 @@ export default function NotebookEditPage() {
     setcategoriesTable,
   ] = useContext(AppContext);
 
+  const notebookSideActions = [
+    {
+      name: 'Toogle View',
+      icon: 'fas fa-eye',
+      action: () => {
+        console.log('action executed');
+      },
+    },
+    {
+      name: 'Insert text',
+      icon: 'fa-text-height',
+      action: () => {
+        console.log('action executed');
+      },
+    },
+    {
+      name: 'Insert image',
+      icon: 'fas fa-image',
+      action: () => {
+        console.log('action executed');
+      },
+    },
+    {
+      name: 'Insert shape',
+      icon: 'fas fa-shapes',
+      action: () => {
+        console.log('action executed');
+      },
+    },
+    {
+      name: 'Insert chart',
+      icon: 'fas fa-chart-pie',
+      action: () => {
+        console.log('action executed');
+      },
+    },
+    {
+      name: 'Draw',
+      icon: 'fas fa-pencil-alt',
+      action: () => {
+        console.log('action executed');
+      },
+    },
+    {
+      name: 'Insert iframe',
+      icon: 'fas fa-laptop-code',
+      action: () => {
+        console.log('action executed');
+      },
+    },
+    {
+      name: 'Insert code',
+      icon: 'fas fa-code',
+      action: () => {
+        console.log('action executed');
+      },
+    },
+    {
+      name: 'Insert table',
+      icon: 'fas fa-table',
+      action: () => {
+        console.log('action executed');
+      },
+    },
+    {
+      name: 'Auto Table of Contents',
+      icon: 'fas fa-th-list',
+      action: () => {
+        console.log('action executed');
+      },
+    },
+    {
+      name: 'Type of paper',
+      icon: 'fas fa-toilet-paper',
+      action: () => {
+        console.log('action executed');
+      },
+    },
+    {
+      name: 'Print',
+      icon: 'fas fa-print',
+      action: () => {
+        console.log('action executed');
+      },
+    },
+    {
+      name: 'Share',
+      icon: 'fas fa-share',
+      action: () => {
+        console.log('action executed');
+      },
+    },
+  ];
   return (
     <div className='notebookPage scrollClass'>
       <aside>
-        main actions{' '}
+        {notebookSideActions.map((item, index) => (
+          <div key={index}>
+            <i className={item.icon}></i>
+            <span>{item.name}</span>
+          </div>
+        ))}
         <svg
           width='52'
           height='60'
@@ -37,21 +136,7 @@ export default function NotebookEditPage() {
       <main>
         <div>notebook</div>
 
-        <div className='controlPageCount'>
-          <button className='skewButton'>
-            <i className='fas fa-backward'></i>
-          </button>
-          <button>
-            <i className='fas fa-angle-left'></i>
-          </button>
-          <input type='number' min={0} />
-          <button>
-            <i className='fas fa-angle-right'></i>
-          </button>
-          <button className='skewButton'>
-            <i className='fas fa-forward'></i>
-          </button>
-        </div>
+        <ControPageCount />
       </main>
     </div>
   );
